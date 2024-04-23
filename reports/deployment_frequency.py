2c9eb491bc22395c93eeadfb5ceab3d160f2b7aa
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-04-23 17:14:15 +0100

    use jinja2 for templates instead of inline

--- reports/deployment_frequency.py
+++ reports/deployment_frequency.py
@@ -2,113 +2,61 @@ import os
 from datetime import date, datetime
 from typing import Any
 from pprint import pp
+from jinja2 import Environment, FileSystemLoader, Template
 
 _path:str = './outputs/reports/deployment_frequency/'
+_templates:str = './reports/templates/deployment_frequency/'
 
 def _by_month(by_month:dict[str, dict[str,Any]]):
-    """"""
-    pre:str =   f"""---
-title: Releases per month
-last_reviewed_on: {datetime.now().strftime('%Y-%m-%d')}
-review_in: 3 months
----
-# <%= current_page.data.title %>
-Releases per month for all OPG owned repositories.
-"""
-    # create headers
-    tbl:str = "| Month | Total | Average |\n| --- | --- | --- |\n"
-    for month, v in by_month.items():
-        tbl += f"| {month} | {v.get('total_success', 0 ) } | {v.get('average_success', 0.0):.2f} | \n"
+    """generate markdown report by month only"""
 
-    content:str = pre + tbl
+    loader:FileSystemLoader = FileSystemLoader(_templates)
+    env:Environment = Environment(loader=loader)
+    template:Template = env.get_template('by_month.erb')
+
+    t:str = datetime.utcnow().strftime('%Y-%m-%d')
+    output:str = template.render(now=t, by_month=by_month)
     os.makedirs(_path, exist_ok=True)
     with open(f'{_path}by_month.md', 'w+') as f:
-        f.write(content)
+        f.write(output)
 
-def _by_repository(by_repo:dict[str, dict[str,Any]], months:list[str]):
-    """"""
-    pre:str =   f"""---
-title: Releases per repository, per month
-last_reviewed_on: {datetime.now().strftime('%Y-%m-%d')}
-review_in: 3 months
----
-# <%= current_page.data.title %>
-Releases per month for each repository.
-"""
-    rlen = 146
-    ilen = 9
-    tbl:str = f"| {'Repository': <{rlen}} | "
-    split:str = f"|:{'':-<{rlen+1}}|"
-    for m in months:
-        tbl += f'{m:>{ilen}} | '
-        split += f"{'':-<{ilen+1}}:|"
-    tbl += '\n'
-    split += '\n'
-    tbl = tbl + split
 
-    totals:dict[str, int] = {}
-    for repo, values in by_repo.items():
-        link:str = f'[{repo}](https://github.com/{repo})'
-        tbl += f'| {link: <{rlen}} |'
-        for m in months:
-            v:dict = values.get(m, {})
-            tbl += f'<span title="average per day: {v.get('average_success', 0.0):.2f}">{v.get('total_success', 0)}</span>  |'
+def _by_repository(by_repo:dict[str, dict[str,Any]], months:list[str], totals:dict[str, dict[str,Any]] ):
+    """generate markdown / html mix for repo per month"""
+    loader:FileSystemLoader = FileSystemLoader(_templates)
+    env:Environment = Environment(loader=loader)
+    template:Template = env.get_template('by_repository.erb')
 
-            if m not in totals:
-                totals[m] = 0
-            totals[m] += v.get('total_success', 0)
-        tbl += '\n'
-    # add totals
-    tbl += f"| {'TOTALS': <{rlen}} |"
-    for m in months:
-        tbl += f" {totals.get(m, 0):>{ilen}} |"
-    tbl += '\n'
-
+    t:str = datetime.utcnow().strftime('%Y-%m-%d')
+    output:str = template.render(now=t, by_repo=by_repo, months=months, totals=totals)
     os.makedirs(_path, exist_ok=True)
     with open(f'{_path}by_repository.md', 'w+') as f:
-        f.write(tbl)
+        f.write(output)
+
 
 
 def _by_teams(by_team:dict[str, dict[str,Any]], months:list[str]):
-    """"""
-    pre:str =   f"""---
-title: Releases per team, per month
-last_reviewed_on: {datetime.now().strftime('%Y-%m-%d')}
-review_in: 3 months
----
-# <%= current_page.data.title %>
-Releases per month for each team.
-"""
-    rlen = 50
-    ilen = 9
-    tbl:str = f"| {'Team': <{rlen}} | "
-    split:str = f"|:{'':-<{rlen+1}}|"
-    for m in months:
-        tbl += f'{m:>{ilen}} | '
-        split += f"{'':-<{ilen+1}}:|"
-    tbl += '\n'
-    split += '\n'
-    tbl = tbl + split
+    """generate markdown / html mix for team per month"""
 
-    for team, values in by_team.items():
-        tbl += f'| {team: <{rlen}} |'
-        for m in months:
-            v:dict = values.get(m, {})
-            tbl += f'<span title="average per day: {v.get('average_success', 0.0):.2f}">{v.get('total_success', 0)}</span>  |'
-        tbl += '\n'
+    loader:FileSystemLoader = FileSystemLoader(_templates)
+    env:Environment = Environment(loader=loader)
+    template:Template = env.get_template('by_team.erb')
 
+    t:str = datetime.utcnow().strftime('%Y-%m-%d')
+    output:str = template.render(now=t, by_team=by_team, months=months)
     os.makedirs(_path, exist_ok=True)
     with open(f'{_path}by_team.md', 'w+') as f:
-        f.write(tbl)
+        f.write(output)
 
 
 
 def report(data:dict[str, dict[str, dict[str,Any]]]):
-    """Generate all the report files"""
+    """Generate all the report files for deployment frequency"""
     # create by_month report
     months:list = data.get('meta').get('months')
     teams: list = data.get('meta').get('teams')
+    by_month = data.get('by_month')
 
-    _by_month(data.get('by_month'))
-    _by_repository(data.get('by_repository'), months)
+    _by_month(by_month)
+    _by_repository(data.get('by_repository'), months, by_month)
     _by_teams(data.get('by_team'), months)
