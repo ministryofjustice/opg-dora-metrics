3c052c867103d9afa71e830a65fa172ecf42a3d1
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2024-05-19 17:41:14 +0100

    add cache

--- app/data/local/github_data/base.py
+++ app/data/local/github_data/base.py
@@ -6,6 +6,9 @@ from app.decorator import timer
 
 G = TypeVar('G', bound=GithubObject)
 
+__cache__:dict = {}
+WITH_CACHE:bool = True
+
 @timer
 def DataMap(source:G, map:dict[str, Callable|None]) -> dict[str, Any]:
     """Use a map of attributes passed to generate a dict of data
@@ -14,6 +17,8 @@ def DataMap(source:G, map:dict[str, Callable|None]) -> dict[str, Any]:
     """
     logging.debug('Data mapping', source=source, map=map)
     raw:dict[str, Any] = {}
+    # caching
+    source_key:str = f'{source.__class__.__name__}:{source.id}'
 
     if source is not None and getattr(source, '__dict__') is not None:
         source_data:dict[str, Any] = vars(source)
@@ -22,15 +27,23 @@ def DataMap(source:G, map:dict[str, Callable|None]) -> dict[str, Any]:
     result: dict = {}
     for attr, f in map.items():
         value:Any = None
+        cache_key:str = f'{source_key}:{attr}'
+        is_cached:bool = cache_key in __cache__.keys()
         is_func:bool = isinstance(f, FunctionType)
-        if is_func:
+        if WITH_CACHE and is_cached:
+            value = __cache__[cache_key]
+            logging.debug(f'CACHED! [{attr}] to [{value}] [cached:{is_cached}]', is_function=is_func, f=f, cache_key=cache_key)
+        elif is_func:
             try:
                 value = f(source)
             except:
                 pass
         else:
             value = raw.get(attr, None)
-        logging.debug(f'mapped [{attr}] to [{value}]', is_function=is_func, f=f)
+        # cache the result
+        if WITH_CACHE:
+            __cache__[cache_key] = value
+        logging.debug(f'mapped [{attr}] to [{value}] [cached:{is_cached}]', is_function=is_func, f=f, cache_key=cache_key)
         result[attr] = value
 
     result['__class__'] = source.__class__.__name__
--- app/data/remote/github/workflow_run.py
+++ app/data/remote/github/workflow_run.py
@@ -11,7 +11,7 @@ from app.decorator import timer
 
 @timer
 def __workflow_runs__(repository:Repository, branch:str, date_range:str, status:str='success') -> list[WorkflowRun]:
-    """Return a list of workflow runs for the repo in the date range set"""    
+    """Return a list of workflow runs for the repo in the date range set"""
     logging.debug('getting workflow runs for date_range', repo=repository.full_name, date_range=date_range, status=status, branch=branch)
     runs:list[WorkflowRun] = [wf for wf in repository.get_workflow_runs(branch=branch, created=date_range, status=status)]
     logging.debug(f'[{repository.full_name}] (workflow_runs) found [{len(runs)}] workflow_runs in date_range',  repo=repository.full_name, date_range=date_range, status=status, branch=branch)
@@ -37,9 +37,9 @@ def workflow_runs(
     logging.debug('date ranges', repo=repository.full_name, start=start, end=end, dates=dates)
 
     for ym in dates:
-        date_range:str = f'{ym}..{ym}'   
-        logging.info(f'[{repository.full_name}] (workflow_runs) getting workflow in range',  repo=repository.full_name, date_range=date_range)     
-        runs:list[WorkflowRun] = __workflow_runs__(repository=repository, branch=branch, 
+        date_range:str = f'{ym}..{ym}'
+        logging.info(f'[{repository.full_name}] (workflow_runs) getting workflow in range',  repo=repository.full_name, date_range=date_range)
+        runs:list[WorkflowRun] = __workflow_runs__(repository=repository, branch=branch,
                                                    date_range=date_range, status=status)
         all += runs
     logging.info(f'[{repository.full_name}] (workflow_runs) found [{len(all)}] overall workflow_runs in range',  repo=repository.full_name, start=start, end=end, status=status, branch=branch)
@@ -55,11 +55,11 @@ def workflow_runs_in_range(start:date,
     upper:datetime = to_datetime(end)
     for wr in workflow_runs:
         if between(test=wr.created_at.replace(tzinfo=timezone.utc), lower=lower, upper=upper):
-            all.append(wr)    
+            all.append(wr)
     return all
 
 @timer
-def matching_workflow_runs(pattern:str, 
+def matching_workflow_runs(pattern:str,
                            workflow_runs:list[WorkflowRun]) -> list[WorkflowRun]:
     """Reduce the list of workflow runs to those that match the pattern"""
     logging.debug('getting workflow runs matching pattern', pattern=pattern)
@@ -67,12 +67,12 @@ def matching_workflow_runs(pattern:str,
     repo:str = None
     total:int = len(workflow_runs)
     for i, wf in enumerate(workflow_runs):
-        if wf.repository is not None and repo is None:        
+        if wf.repository is not None and repo is None:
             repo = wf.repository.full_name
         pattern_match:bool = bool(re.search(pattern, wf.name.lower())) if pattern is not None else True
-        flag:str = "✅" if pattern_match else "❌" 
-        logging.debug(f'[{repo}] (workflow_runs) [{i}+1/{total}] workflow pattern matched {flag}', pattern=pattern, workflow_run=wf.id, workflow_name=wf.name)     
-        if pattern_match:            
+        flag:str = "✅" if pattern_match else "❌"
+        logging.debug(f'[{repo}] (workflow_runs) [{i+1}/{total}] workflow pattern matched {flag}', pattern=pattern, workflow_run=wf.id, workflow_name=wf.name)
+        if pattern_match:
             matched.append(wf)
     logging.info(f'[{repo}] (workflow_runs) found [{len(matched)}] that match pattern', pattern=pattern)
-    return matched 
+    return matched
